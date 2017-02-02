a19d0a2400fbaa6945fd2b06baadb1d151af3314
sebsel <sebsel@seblog.nl>
2017-02-03 00:15:03 +0100

    more consistency + can't regex html
    I'm going for 'good enough' here.

--- lib/webmention.php
+++ lib/webmention.php
@@ -19,30 +19,25 @@ class Webmention {
 
     if(isset($headers['link'])) {
       foreach(explode(',', $headers['link']) as $link) {
-        if(preg_match('!\<(.*?)\>;\s*rel="?(.*?\s)?webmention(\s.*?)?"?!', $link, $match)) {
-
-          $endpoint = url::makeAbsolute($match[1], $url);
-
-          // return valid endpoint or continue searching
-          if(v::url($endpoint)) return $endpoint;
-        }
-      }
-    }
-
-    if(preg_match_all('!\<(a|link)(.*?)\>!i', $html, $links)) {
-
-      foreach($links[0] as $link) {
-
-        if(!preg_match('!rel="(.*?\s)?webmention(\s.*?)?"!', $link)) continue;
-        if(!preg_match('!href="(.*?)"!', $link, $match)) continue;
-        if($match[1] == "") return $url;
+        if(!preg_match('!\<(.*?)\>;\s*rel="?(.*?\s)?webmention(\s.*?)?"?!', $link, $match)) continue;
 
         $endpoint = url::makeAbsolute($match[1], $url);
 
-        // invalid endpoint
-        if(!v::url($endpoint)) continue;
+        // return valid endpoint or continue searching
+        if(v::url($endpoint)) return $endpoint;
+      }
+    }
 
-        return $endpoint;
+    // You can't parse HTML with regex
+    if(preg_match_all('!\<(a|link)(.*?)\>!i', $html, $links)) {
+      foreach($links[0] as $link) {
+        if(!preg_match('!\srel\s*=\s*["\'](.*?\s)?webmention(\s.*?)?["\']!', $link)) continue;
+        if(!preg_match('!\shref\s*=\s*["\'](.*?)["\']!', $link, $match)) continue;
+
+        $endpoint = url::makeAbsolute($match[1], $url);
+
+        // return valid endpoint
+        if(v::url($endpoint)) return $endpoint;
       }
     }
 
